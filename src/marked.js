0d23d5e438a048ee496a05ca78e858785c6f5b38
TigerZ <zhangyihu@gmail.com>
2018-04-06 01:24:19 +0800

    init

--- src/marked.js
+++ src/marked.js
@@ -13,7 +13,7 @@ renderer.link = function (href, title, text) {
         const name = title.substr(prefix.length);
         return `<div>${text}</div><div id="${name}" style="border:1px solid rgba(0,0,0,0.3);background-color:rgba(255,255,0,0.2);width:320px;height:240px"></div><script src="${href.replace(/html/g, 'js')}?_t=${+new Date()}"></script>`;
     } else if (href && href.endsWith('md')) {
-        return `<a title="${title}" href="#${href}?_t=${+new Date()}">${text}</a>`;
+        return `<a title="${title}" href="#${href}">${text}</a>`;
     }
     return `<a href="${href}" title="${title}">${text}</a>`;
 };
@@ -27,7 +27,7 @@ myMarked.setOptions({
 
 function openmd(url, id) {
     $(`#${id}`).html('正在装入数据，请稍等...');
-    $.get(`${url}?${+new Date()}`, (md) => {
+    $.get(`${url}?_t=${+new Date()}`, (md) => {
         const result = myMarked(md, { renderer });
         $(`#${id}`).html(result);
     });
